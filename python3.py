d059f7a0cdd320a651285cf9378ff53397d4ca5e
SMurano88 <sandra.martinez@ieslania.es>
2024-12-02 20:03:13 +0100

    Home2D

---
+++ python3.py
@@ -0,0 +1,9 @@
+
+nombre = input("Dame tu nombre: ")
+print("Hola, ", nombre)
+
+n1 = int(input("Dame un número "))
+n2 = int(input("Dame otro número "))
+
+print("Su suma es ", n1+n2)
+print("Su multiplicación es ", n1*n2)
