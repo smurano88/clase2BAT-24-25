493918ac03ba94127ca15fdea0ef8c543ce7c199
SMurano88 <sandra.martinez@ieslania.es>
2024-12-06 19:15:05 +0100

    Home6D

---
+++ python5.py
@@ -0,0 +1,18 @@
+dia = int(input("Dame un número: "))
+
+if dia == 1 :
+    print("Lunes")
+elif dia == 2 :
+    print("Martes")
+elif dia == 3 :
+    print("Miércoles")
+elif dia == 4 :
+    print("Jueves")
+elif dia == 5 :
+    print("Viernes")
+elif dia == 6 :
+    print("Sábado")
+elif dia == 7 :
+    print("Domingo")
+else :
+    print("Fuera rango")
