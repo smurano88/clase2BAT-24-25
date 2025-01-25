85f6956ff606710c91e2755098179a6947d6e673
SMurano88 <sandra.martinez@ieslania.es>
2025-01-25 18:56:44 +0100

    Home25E

---
+++ navidad2.py
@@ -0,0 +1,25 @@
+# árbol de Navidad del IES La Nía alumnos de PRySI II ;)
+
+#Líneas en blanco inicio
+numero_de_lineas = 5
+for _ in range(numero_de_lineas):
+    print()
+
+print ("               *               ")
+print ("              ***               ")
+print ("             *****             ")
+print ("            *******                     ***  ***  *    *  ***      **  *   *  *   * *  ***    *   ***           ")
+print ("           *********                    **   *    *    *   **      * * *  ***  * *  *  *  *  ***  *  *          ")
+print ("          ***********                   *    ***  ***  *  ***      *  ** *   *  *   *  ***  *   * ** *           " )
+print ("         *************            ")
+print ("        ***************            ")
+print ("             *****             ")
+print ("             *****             ")
+print ("             *****             ")
+
+
+
+#Líneas en blanco fin
+numero_de_lineas = 5
+for _ in range(numero_de_lineas):
+    print()
